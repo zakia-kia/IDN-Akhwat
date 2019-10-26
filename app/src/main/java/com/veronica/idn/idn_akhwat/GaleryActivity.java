b1001baca2d5777a57b69112d3e60115bfe6a0ca
mac <putyasekarayu@gmail.com>
2019-10-26 17:10:23 +0700

    Add new galery activity

---
+++ app/src/main/java/com/veronica/idn/idn_akhwat/GaleryActivity.java
@@ -0,0 +1,14 @@
+package com.veronica.idn.idn_akhwat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GaleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galery);
+    }
+}
