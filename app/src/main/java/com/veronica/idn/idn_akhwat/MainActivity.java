d9341b8babb0413439ed91c470665c741d457260
veroanggra <veronicaputri497@gmail.com>
2019-10-26 14:29:32 +0700

    first stage

---
+++ app/src/main/java/com/veronica/idn/idn_akhwat/MainActivity.java
@@ -0,0 +1,14 @@
+package com.veronica.idn.idn_akhwat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
