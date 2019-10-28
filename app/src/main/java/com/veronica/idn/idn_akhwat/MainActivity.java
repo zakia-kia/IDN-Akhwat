090ef953eb08af59cf6fb76e30d6d4f059a02776
muthia665 <muthia09876@gmail.com>
2019-10-28 15:08:59 +0700

    membuat navigation bar

--- app/src/main/java/com/veronica/idn/idn_akhwat/MainActivity.java
+++ app/src/main/java/com/veronica/idn/idn_akhwat/MainActivity.java
@@ -1,14 +1,101 @@
 package com.veronica.idn.idn_akhwat;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawerLayout,toolbar,R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null){
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                    new MessageFragment()).commit();
+//            navigationView.setCheckedItem(R.id.nav_message);
+        }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+
+            case R.id.nav_article:
+                Toast.makeText(this, "Article", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_profil:
+                Toast.makeText(this, "Profil", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_gallery:
+                Toast.makeText(this, "Gallery", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_event:
+                Toast.makeText(this, "Event", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_program:
+                Toast.makeText(this, "Program", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_curriculum:
+                Toast.makeText(this, "Curriculum", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_schedule:
+                Toast.makeText(this, "Schedule", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_tracking:
+                Toast.makeText(this, "Tracking", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 }
